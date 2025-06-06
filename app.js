56be741eb81bbd9a8c5776de621e0fe23e09994d
Luis Enrique Lopez Cortes <luis.lopezcortes@uanl.edu.mx>
2025-06-06 08:24:07 -0600

    Cambio para urlencoded

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
 const cors = require('cors');
-const registerRoutes = require('./routes/register');
 require('dotenv').config();
+
+const registerRoutes = require('./routes/register');
+const loginRoutes = require('./routes/login');
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // Para JSON
+app.use(express.urlencoded({ extended: true })); // Para x-www-form-urlencoded
+
 app.use('/uploads', express.static('uploads'));
 app.use('/api/register', registerRoutes);
-
-const loginRoutes = require('./routes/login');
 app.use('/api/login', loginRoutes);
 
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
-
 
 
 // routes/register.js
