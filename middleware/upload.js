885c425117a12177bac638afed76402a468c04a4
Luis Enrique Lopez Cortes <luis.lopezcortes@uanl.edu.mx>
2025-06-07 09:56:14 -0600

    cambios a Notas 2.0

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,15 +1,23 @@
-// middlewares/upload.js
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+// Asegura que la carpeta exista
+const uploadPath = 'uploads/notas';
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-  destination: 'uploads/notas/',
+  destination: (req, file, cb) => {
+    cb(null, uploadPath);
+  },
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
-    const uniqueName = `nota_${Date.now()}${ext}`;
-    cb(null, uniqueName);
+    cb(null, Date.now() + ext);
   }
 });
 
 const upload = multer({ storage });
+
 module.exports = upload;
