9c647b733a2d48f688d082ad24cca35b54e9adaf
Luis Enrique Lopez Cortes <luis.lopezcortes@uanl.edu.mx>
2025-06-07 10:00:35 -0600

    cambios a Notas 2.5

--- routes/notas.js
+++ routes/notas.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const notaController = require('../controllers/notasController');
-const upload = require('../middlewares/upload');
+const upload = require('../middleware/upload');
 
 router.post('/agregarNota', upload.single('imagen'), notaController.agregarNota);
 
